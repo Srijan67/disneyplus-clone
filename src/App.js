f11df51be8af6e8909f69c73faad62bed03ca147
Srijan Sinha <srijan@scizers.in>
2022-10-01 17:17:22 +0530

    single movie screen

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Details from "./components/Details";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Login from "./components/Login";
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/home" element={<Home />} />
+          <Route path="/detail/:id" element={<Details />} />
         </Routes>
       </BrowserRouter>
     </div>
